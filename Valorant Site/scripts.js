97d9adb588f3160d85828128d75c5d6f40535a09
Amirson16 <75690951+Amirson16@users.noreply.github.com>
2025-06-04 15:01:47 -0300

    Delete Valorant Site directory

--- Valorant Site/scripts.js
+++
@@ -1,8 +0,0 @@
-let big = document.querySelector(".img_grande img")
-let minis = document.querySelectorAll(".img_pequena img")
-
-minis.forEach(item => {
-    item.addEventListener("click", () => {
-        big.src = item.src
-    })
-})
